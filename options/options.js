596fec49ce76565039bedd58f3791e976e670542
Crowdbotics <team@crowdbotics.com>
2022-08-16 13:04:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tiki_king_live_36810",
+  url: "https://tiki_king_live_36810.botics.co",
+  api: "https://tiki_king_live_36810.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
